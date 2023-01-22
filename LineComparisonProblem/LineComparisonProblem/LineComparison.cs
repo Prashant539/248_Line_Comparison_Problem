7544839ddd61bf1a388a262870a2ecb4f56d2c68
Prashant <prashant.chaurasiya.cp@gmail.com>
2023-01-22 16:40:17 +0530

    [Prashant]Add: Checking Equality of Lines

--- LineComparisonProblem/LineComparisonProblem/LineComparison.cs
+++ LineComparisonProblem/LineComparisonProblem/LineComparison.cs
@@ -22,9 +22,33 @@ namespace LineComparisonProblem
             Console.WriteLine("Enter the Second Co-ordinate y2: ");
             int y2 = Convert.ToInt32(Console.ReadLine());
 
-            var Length =Math.Sqrt((Math.Pow(x1-x2, 2) +Math.Pow(y2 - y1, 2)));
-            Console.WriteLine("Length of line : " + Length);
+            var Length1 =Math.Sqrt((Math.Pow(x1-x2, 2) +Math.Pow(y2 - y1, 2)));
+            Console.WriteLine("Length of line : " + Length1);
 
+            Console.WriteLine("Enter the third Co-ordinate x3: ");
+            int x3 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the Fourth Co-ordinate x4: ");
+            int x4 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the third Co-ordinate y3: ");
+            int y3 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the third Co-ordinate y4: ");
+            int y4 = Convert.ToInt32(Console.ReadLine());
+
+            var Length2 = Math.Sqrt((Math.Pow(x3 - x4, 2) + Math.Pow(y3 - y4, 2)));
+            Console.WriteLine("Length of line : " + Length2);
+
+            if(Length1 == Length2)
+            {
+                Console.WriteLine("The Two Lines are Equal");
+
+            }
+            else
+            {
+                Console.WriteLine("The Two Lines are not Equal");
+            }
 
 
         }
