2e7ea31f534534d6ea2466da1a8052bd8a3d77a2
Prashant <prashant.chaurasiya.cp@gmail.com>
2023-01-22 16:14:14 +0530

    [Prashant]Adding: Co-ordinates of line and claculate length of line

---
+++ LineComparisonProblem/LineComparisonProblem/LineComparison.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparisonProblem
+{
+    public class LineComparison
+    {
+        public void LineLength()
+        {
+            Console.WriteLine("Enter the First Co-ordinate x1: ");
+            int x1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the Second Co-ordinate x2: ");
+            int x2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the First Co-ordinate y1: ");
+            int y1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the Second Co-ordinate y2: ");
+            int y2 = Convert.ToInt32(Console.ReadLine());
+
+            var Length =Math.Sqrt((Math.Pow(x1-x2, 2) +Math.Pow(y2 - y1, 2)));
+            Console.WriteLine("Length of line : " + Length);
+
+
+
+        }
+    }
+}
---
+++ LineComparisonProblem/LineComparisonProblem/Program.cs
@@ -0,0 +1,14 @@
+﻿using LineComparisonProblem;
+using System;
+namespace LineComparisomProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Line Comparison Problem");
+            LineComparison lineComparison = new LineComparison();
+            lineComparison.LineLength();
+        }
+    }
+}
